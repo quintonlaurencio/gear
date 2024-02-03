257aaf6bb6d14d0f7ea2a721f1d3ba72b02d1926
QL <q.n.laurencio@gmail.com>
2024-02-03 15:14:38 -0500

    Initial Commit

---
+++ Gear/GearApp.swift
@@ -0,0 +1,17 @@
+//
+//  GearApp.swift
+//  Gear
+//
+//  Created by Quinton Laurencio on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
