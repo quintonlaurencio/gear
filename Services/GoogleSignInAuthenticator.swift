16522f588903285e66f8d9fd189cd46572a4fde9
QL <q.n.laurencio@gmail.com>
2024-03-25 01:21:03 -0400

    removed birthday from Services/GoogleSignInAuthenticator.swift and ViewModels/AuthenticationViewModel.swift and something changed in Gear.xcodeproj/project.pbxproj

--- Services/GoogleSignInAuthenticator.swift
+++ Services/GoogleSignInAuthenticator.swift
@@ -19,109 +19,60 @@ import GoogleSignIn
 
 /// An observable class for authenticating via Google.
 final class GoogleSignInAuthenticator: ObservableObject {
-  private var authViewModel: AuthenticationViewModel
-
-  /// Creates an instance of this authenticator.
-  /// - parameter authViewModel: The view model this authenticator will set logged in status on.
-  init(authViewModel: AuthenticationViewModel) {
-    self.authViewModel = authViewModel
-  }
-
-  /// Signs in the user based upon the selected account.'
-  /// - note: Successful calls to this will set the `authViewModel`'s `state` property.
-  func signIn() {
-#if os(iOS)
-    guard let rootViewController = UIApplication.shared.windows.first?.rootViewController else {
-      print("There is no root view controller!")
-      return
-    }
-
-    GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
-      guard let signInResult = signInResult else {
-        print("Error! \(String(describing: error))")
-        return
-      }
-      self.authViewModel.state = .signedIn(signInResult.user)
-    }
-
-#elseif os(macOS)
-    guard let presentingWindow = NSApplication.shared.windows.first else {
-      print("There is no presenting window!")
-      return
-    }
-
-    GIDSignIn.sharedInstance.signIn(withPresenting: presentingWindow) { signInResult, error in
-      guard let signInResult = signInResult else {
-        print("Error! \(String(describing: error))")
-        return
-      }
-      self.authViewModel.state = .signedIn(signInResult.user)
-    }
-#endif
-  }
-
-  /// Signs out the current user.
-  func signOut() {
-    GIDSignIn.sharedInstance.signOut()
-    authViewModel.state = .signedOut
-  }
-
-  /// Disconnects the previously granted scope and signs the user out.
-  func disconnect() {
-    GIDSignIn.sharedInstance.disconnect { error in
-      if let error = error {
-        print("Encountered error disconnecting scope: \(error).")
-      }
-      self.signOut()
-    }
-  }
-
-  // Confines birthday calucation to iOS for now.
-  /// Adds the birthday read scope for the current user.
-  /// - parameter completion: An escaping closure that is called upon successful completion of the
-  /// `addScopes(_:presenting:)` request.
-  /// - note: Successful requests will update the `authViewModel.state` with a new current user that
-  /// has the granted scope.
-  func addBirthdayReadScope(completion: @escaping () -> Void) {
-    guard let currentUser = GIDSignIn.sharedInstance.currentUser else {
-      fatalError("No user signed in!")
+    private var authViewModel: AuthenticationViewModel
+    
+    /// Creates an instance of this authenticator.
+    /// - parameter authViewModel: The view model this authenticator will set logged in status on.
+    init(authViewModel: AuthenticationViewModel) {
+        self.authViewModel = authViewModel
     }
     
-    #if os(iOS)
-    guard let rootViewController = UIApplication.shared.windows.first?.rootViewController else {
-      fatalError("No root view controller!")
+    /// Signs in the user based upon the selected account.'
+    /// - note: Successful calls to this will set the `authViewModel`'s `state` property.
+    func signIn() {
+#if os(iOS)
+        guard let rootViewController = UIApplication.shared.windows.first?.rootViewController else {
+            print("There is no root view controller!")
+            return
+        }
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
+            guard let signInResult = signInResult else {
+                print("Error! \(String(describing: error))")
+                return
+            }
+            self.authViewModel.state = .signedIn(signInResult.user)
+        }
+        
+#elseif os(macOS)
+        guard let presentingWindow = NSApplication.shared.windows.first else {
+            print("There is no presenting window!")
+            return
+        }
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingWindow) { signInResult, error in
+            guard let signInResult = signInResult else {
+                print("Error! \(String(describing: error))")
+                return
+            }
+            self.authViewModel.state = .signedIn(signInResult.user)
+        }
+#endif
     }
-
-    currentUser.addScopes([BirthdayLoader.birthdayReadScope],
-                          presenting: rootViewController) { signInResult, error in
-      if let error = error {
-        print("Found error while adding birthday read scope: \(error).")
-        return
-      }
-
-      guard let signInResult = signInResult else { return }
-      self.authViewModel.state = .signedIn(signInResult.user)
-      completion()
+    
+    /// Signs out the current user.
+    func signOut() {
+        GIDSignIn.sharedInstance.signOut()
+        authViewModel.state = .signedOut
     }
-
-    #elseif os(macOS)
-    guard let presentingWindow = NSApplication.shared.windows.first else {
-      fatalError("No presenting window!")
+    
+    /// Disconnects the previously granted scope and signs the user out.
+    func disconnect() {
+        GIDSignIn.sharedInstance.disconnect { error in
+            if let error = error {
+                print("Encountered error disconnecting scope: \(error).")
+            }
+            self.signOut()
+        }
     }
-
-    currentUser.addScopes([BirthdayLoader.birthdayReadScope],
-                          presenting: presentingWindow) { signInResult, error in
-      if let error = error {
-        print("Found error while adding birthday read scope: \(error).")
-        return
-      }
-
-      guard let signInResult = signInResult else { return }
-      self.authViewModel.state = .signedIn(signInResult.user)
-      completion()
-    }
-
-    #endif
-  }
-
 }
